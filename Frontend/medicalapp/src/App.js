78d4caaab310d6b3d494ce51ca598088dd3ea27b
Oumaima Souguir <81293137+Souguiroumaima7@users.noreply.github.com>
2023-05-23 11:22:53 +0100

    commited in app directory and make routers

--- Frontend/medicalapp/src/App.js
+++ Frontend/medicalapp/src/App.js
@@ -1,22 +1,34 @@
 
-import Header from '../src/Compoments/Header';
+import Header from './Compoments/Header';
 import './App.css';
 import { Container } from "@material-ui/core";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Service from '../src/Compoments/Service' ;
-import About from '../src/Compoments/About' ;
+import Service from './Compoments/Service' ;
+import About from './Compoments/About' ;
+import Feature from './Compoments/Feature' ;
+import Team from './Compoments/Team' ;
+import Testimonial from './Compoments/Testimonial' ;
+import NavBar from './Compoments/NabBar' ;
+import Appointement from './Compoments/Appointement' ;
+
 function App() {
   return (
-
+    <Router>
     <div className="App">
       <Header /> 
         <Container>
           <Switch>
+          <Route path="/" component={NavBar} exact />
           <Route path="/" component={About} exact />
+          <Route path ="/" component ={Appointement} exact />
           <Route path="/" component={Service} exact />
+          <Route path = "/" component={Feature} exact />
+          <Route path = "/" component={Team} exact />
+          <Route path = "/" component={Testimonial} exact />
             </Switch>
            </Container> 
     </div>
+    </Router>
   );
 }
 
